f6961cf71b3c11c838b9b04cd71c313df31e38ba
sepuckett86 <sepuckett86@gmail.com>
2018-04-17 13:49:08 +0200

    added a container, tried adding d3 graph, still some issues

--- ccompare/src/Components/App/App.js
+++ ccompare/src/Components/App/App.js
@@ -3,105 +3,21 @@ import './App.css';
 
 import Compare from '../Compare/Compare';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      menus: [
-        {
-          id: '1',
-          items: ['A', 'B', 'C'],
-          active: 'A',
-          key: 'menu1'
-        },
-        {
-          id: '2',
-          items: ['A', 'B', 'C'],
-          active: 'A',
-          key: 'menu2'
-        }
-      ],
-      showResult: false,
-      sites: ['Apples', 'Oranges', 'Bananas'],
-      results: [
-        {
-          site: 'Bananas',
-          items: ['A', 'B'],
-          result: 'AB Bananas'
-        },
-        {
-          site: 'Bananas',
-          items: ['A', 'C'],
-          result: 'AC Bananas'
-        },
-        {
-          site: 'Bananas',
-          items: ['B', 'C'],
-          result: 'BC Bananas'
-        },
-        {
-          site: 'Oranges',
-          items: ['A', 'B'],
-          result: 'AB Oranges'
-        },
-        {
-          site: 'Oranges',
-          items: ['A', 'C'],
-          result: 'AC Oranges'
-        },
-        {
-          site: 'Oranges',
-          items: ['B', 'C'],
-          result: 'BC Oranges'
-        },
-        {
-          site: 'Apples',
-          items: ['A', 'B'],
-          result: 'AB Apples'
-        },
-        {
-          site: 'Apples',
-          items: ['A', 'C'],
-          result: 'AC Apples'
-        },
-        {
-          site: 'Apples',
-          items: ['B', 'C'],
-          result: 'BC Apples'
-        },
-      ]
-    }
-
-    this.updateActive = this.updateActive.bind(this);
-    this.updateResult = this.updateResult.bind(this);
+export class App extends Component {
+  displayName: 'App';
+  propTypes: {
+    menus: PropTypes.array,
+    sites: PropTypes.array,
+    results: PropTypes.array,
+    updateResult: PropTypes.func,
+    showResult: PropTypes.bool,
+    updateActive: PropTypes.func
   }
-
-  updateActive(id, active) {
-    let menus = this.state.menus;
-    for (let i=0; i<this.state.menus.length; i++) {
-      if (this.state.menus[i].id === id) {
-        menus[i].active = active;
-        this.setState({
-          menus: menus
-        })
-      }
-    }
-  }
-
-  updateResult() {
-    if (this.state.showResult === false) {
-      this.setState({
-        showResult: true
-      })
-    }
-  }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <br /><br />
-          <h1 className="App-title">Welcome to CCompare</h1>
+          <div className="App-title">CCompare</div>
         </header>
         <p className="App-intro">
           <br />
@@ -109,12 +25,12 @@ class App extends Component {
         </p>
         <div className="container">
           <Compare
-            updateMenu={this.updateActive}
-            menus={this.state.menus}
-            showResult={this.state.showResult}
-            updateResult={this.updateResult}
-            sites={this.state.sites}
-            results={this.state.results} />
+            updateMenu={this.props.updateActive}
+            menus={this.props.menus}
+            showResult={this.props.showResult}
+            updateResult={this.props.updateResult}
+            sites={this.props.sites}
+            results={this.props.results} />
         </div>
 
       </div>
--- ccompare/src/Components/Result/Result.js
+++ ccompare/src/Components/Result/Result.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './Result.css';
+import Graph from '../Graph/Graph';
 
 class Result extends React.Component {
+  // Look up data for particular site
   checkActive(site) {
+    // Define what user wants to compare
     let menu1 = this.props.menus[0];
     let menu2 = this.props.menus[1];
+    // Entire data set
     let results = this.props.results;
-    for(let i=0; i<results.length; i++) {
-      if(results[i].site === site) {
-        if(results[i].items.includes(menu1.active)
+    // Cycle through entire data set looking for a match
+    for (let i = 0; i < results.length; i++) {
+      if (results[i].site === site) {
+        if (results[i].items.includes(menu1.active)
         && results[i].items.includes(menu2.active)) {
-          return (<p>{results[i].result}</p>)
+          return results[i].result;
         }
       }
     }
@@ -19,33 +24,52 @@ class Result extends React.Component {
   displayResult() {
     let menu1 = this.props.menus[0];
     let menu2 = this.props.menus[1];
+    // Make array of data
+    let data = [];
+    this.props.sites.forEach(site => {
+      data.push(this.checkActive(site));
+    })
+
     if(menu1.active === menu2.active) {
       return <p>Pick two different options</p>
     } else {
             return(
-              <div>
+                <div>
                 <p>Comparing {menu1.active} + {menu2.active}:</p>
+                <div className="row">
+
+                <div className='col'>
                 <table className="table">
                   <thead>
                 <tr>
-                  <th scope="col">Fruit</th>
+                  <th scope="col">Site</th>
                   <th scope="col">Result</th>
                 </tr>
               </thead>
-              <tbody>
+                <tbody>
                 {
                   this.props.sites.map(site => {
                     return <tr key={site}>
                          <th scope="row">{site}</th>
-                         <td>{this.checkActive(site)}
+                         <td><p>{this.checkActive(site)}</p>
                            </td>
-
                        </tr>
                      })
                    }
-             </tbody>
-           </table>
-         </div>)
+                 </tbody>
+                </table>
+              </div>
+
+         <div className='col'>
+           <Graph
+              results={this.props.results}
+              sites={this.props.sites}
+              menu1={this.props.menus[0]}
+              menu2={this.props.menus[1]}
+              />
+         </div>
+       </div>
+     </div>)
           }
   }
 
