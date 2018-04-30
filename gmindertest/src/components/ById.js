bebc00a9e2752459cbfc4752cff8e15a97147a95
sepuckett86 <sepuckett86@gmail.com>
2018-04-30 14:05:48 +0200

    add byId functionality

---
+++ gmindertest/src/components/ById.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class ById extends Component {
+
+  render() {
+    const gminder = this.props.gminder;
+    return (
+        <div className="container">
+        <p>{gminder.mainResponse}</p>
+        <p>{gminder.author}</p>
+        <p>{gminder.reason}</p>
+        </div>
+    );
+  }
+}
+
+export default ById;
