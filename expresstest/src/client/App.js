c2ad695ddbe8d0e0c5dffd0f840fcef0af48ec71
sepuckett86 <sepuckett86@gmail.com>
2018-05-07 18:21:40 +0200

    added some buttons, get works but put does not

--- expresstest/src/client/App.js
+++ expresstest/src/client/App.js
@@ -1,7 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
+import ExpressTest from './utils/ExpressTest';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      database: 'no data',
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+
+  }
+
+  handleChange(event) {
+    if(event.target.id === 'putInput') {
+      this.setState({
+        input: event.target.value
+      })
+
+    }
+  }
+
+  handleClick(event) {
+    if(event.target.id === 'get') {
+      ExpressTest.getDatabase().then(database => {
+        const newData = JSON.stringify(database);
+        this.setState({
+          database: newData
+        })
+      })
+    }
+    if(event.target.id === 'put') {
+      ExpressTest.putDatabase(this.state.input).then(database => {
+        this.setState({
+          database: database
+        })
+      })
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -9,8 +48,19 @@ class App extends Component {
           <h1 className="App-title">expresstest</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Press a button to interact with server.
         </p>
+        <button id="get" onClick={this.handleClick}>GET</button> <br />
+        This works
+        <br />
+        <br /><br />
+        <input id="putInput" onChange={this.handleChange}/>
+        <button id="put" onClick={this.handleClick}>PUT</button> <br />
+        This does not work
+        <br />
+        <div className="container">
+          {this.state.database}
+        </div>
       </div>
     );
   }
