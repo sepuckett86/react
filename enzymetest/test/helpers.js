9bff2f582c35c56504cd903eba0bddacd6cec7b4
sepuckett86 <sepuckett86@gmail.com>
2018-05-08 10:45:11 +0200

    first enzyme test

---
+++ enzymetest/test/helpers.js
@@ -0,0 +1,16 @@
+
+import { expect } from 'chai';
+import { sinon, spy } from 'sinon';
+import { mount, render, shallow } from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+
+configure({ adapter: new Adapter() });
+
+global.expect = expect;
+global.sinon = sinon;
+global.spy = spy;
+
+global.mount = mount;
+global.render = render;
+global.shallow = shallow;
